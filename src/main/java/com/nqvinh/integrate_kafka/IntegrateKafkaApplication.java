0908773a2212c2622f1177a73fdbfe221b1f77f6
Nqvinh7603 <nqvinh7603@gmail.com>
2024-10-23 00:48:49 +0700

    init project

---
+++ src/main/java/com/nqvinh/integrate_kafka/IntegrateKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.nqvinh.integrate_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrateKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrateKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nqvinh/integrate_kafka/IntegrateKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nqvinh.integrate_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrateKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
